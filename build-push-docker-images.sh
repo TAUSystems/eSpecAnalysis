b90e0aa18d1fd86cacb1c6d8635875dbc1590c5b
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-01-31 16:05:08 -0800

    Tag Docker image v1.0.1

--- build-push-docker-images.sh
+++ build-push-docker-images.sh
@@ -3,6 +3,8 @@
 # if necessary, reauthenticate:
 # aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 460578213037.dkr.ecr.us-west-1.amazonaws.com
 
+export IMAGE_BACKEND_DOCKER_TAG=v1.0.1
+
 docker build -t image-backend-especanalysis .
-docker tag image-backend-especanalysis 460578213037.dkr.ecr.us-west-1.amazonaws.com/tau-image-processing-backend/especanalysis:latest
-docker push 460578213037.dkr.ecr.us-west-1.amazonaws.com/tau-image-processing-backend/especanalysis:latest
+docker tag image-backend-especanalysis 460578213037.dkr.ecr.us-west-1.amazonaws.com/tau-image-processing-backend/especanalysis:$IMAGE_BACKEND_DOCKER_TAG
+docker push 460578213037.dkr.ecr.us-west-1.amazonaws.com/tau-image-processing-backend/especanalysis:$IMAGE_BACKEND_DOCKER_TAG
