4db72e187240a168d3084809ffecfa465045ccda
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-02-08 17:00:24 -0800

    v1.0.2

--- build-push-docker-images.sh
+++ build-push-docker-images.sh
@@ -3,7 +3,7 @@
 # if necessary, reauthenticate:
 # aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 460578213037.dkr.ecr.us-west-1.amazonaws.com
 
-export IMAGE_BACKEND_DOCKER_TAG=v1.0.1
+export IMAGE_BACKEND_DOCKER_TAG=v1.0.2
 
 docker build -t image-backend-especanalysis .
 docker tag image-backend-especanalysis 460578213037.dkr.ecr.us-west-1.amazonaws.com/tau-image-processing-backend/especanalysis:$IMAGE_BACKEND_DOCKER_TAG
